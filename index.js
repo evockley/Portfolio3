cfd4d66f57bf51df04bf474014747615f52a1917
evockley <77511461+evockley@users.noreply.github.com>
2021-06-25 19:55:34 -0400

    6/25/21
    began adding assets to recycle easy case study.
    added bootstrap components

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-$(".toggle").on("click", function(){
+$(".toggle").on("click", function () {
     $(".nav-item").css("display", "flex");
     $(".close").css("display", "flex");
     $(this).css("display", "none");
 });
 
-$(".close").on("click", function(){
+$(".close").on("click", function () {
     $(".nav-item").css("display", "none");
     $(".toggle").css("display", "flex");
     $(this).css("display", "none");
 });
 
+
+// RECYCLE EASY JOURNEY MAP / LINE GRAPH
+
+var xValues = [1, 2, 3, 4, 5, 6, 7, 8];
+var yValues = [8, 4, 6, 5, 7, 8, 9, 10]
+var journeyMap = new Chart("journeyMap", {
+    type: "line",
+    data: {
+
+                labels: xValues,
+                datasets: [{
+                    backgroundColor: "#33333320",
+                    borderColor: "#333",
+                    data: yValues,
+                    lineTension: 0.14,
+                    pointRadius: 8,
+                    pointBackgroundColor: "#5cb075",
+                    pointBorderColor: "#333",
+                    pointHoverBackgroundColor: "#333",
+                    pointHoverRadius: 16,
+                }]
+            },
+  });
