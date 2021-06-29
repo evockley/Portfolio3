a69e2980d8d3150358f6baa6b0a30f0a012aafb1
evockley <77511461+evockley@users.noreply.github.com>
2021-06-29 15:28:33 -0400

    6/29/21
    working on recycle easy case study

--- index.js
+++ index.js
@@ -14,22 +14,35 @@ $(".close").on("click", function () {
 // RECYCLE EASY JOURNEY MAP / LINE GRAPH
 
 var xValues = [1, 2, 3, 4, 5, 6, 7, 8];
-var yValues = [8, 4, 6, 5, 7, 8, 9, 10]
+var yValues = [8, 2, 6, 5, 7, 8, 9, 10]
 var journeyMap = new Chart("journeyMap", {
     type: "line",
     data: {
-
-                labels: xValues,
+                labels: ["Step 1", "Step 2", "Step 3", "Step 4", "Step 5", "Step 6", "Step 7", "Step 8"],
                 datasets: [{
-                    backgroundColor: "#33333320",
+                    backgroundColor: "#33333315",
                     borderColor: "#333",
                     data: yValues,
-                    lineTension: 0.14,
-                    pointRadius: 8,
+                    lineTension: 0.2,
+                    pointRadius: 10,
                     pointBackgroundColor: "#5cb075",
                     pointBorderColor: "#333",
                     pointHoverBackgroundColor: "#333",
-                    pointHoverRadius: 16,
+                    pointHoverRadius: 18,
+
                 }]
             },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+
   });
+
+
+//   RECYCLE EASY CAROUSEL
