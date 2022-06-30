7b930bb2cb2a3423adbdfe92c4a1682c40cc433f
evockley <77511461+evockley@users.noreply.github.com>
2022-06-29 22:06:51 -0400

    6/29/22

--- index.js
+++ index.js
@@ -37,7 +37,7 @@
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop >= 80) {
         document.getElementById("navbarRegular").style.padding = "0px 24px";
 
     } else {
